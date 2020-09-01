b1e37b8daa570f75fdb34f8a33691458a9a8d42d
velveet <velveet@pm.me>
2020-09-01 18:15:02 -0500

    components folder added. Component1 created & styled.

---
+++ src/components/Component1.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Component1() {
+    return (
+        <div>
+            <h1>Component 1</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur facilis eum sequi officia unde quis odit, rem et odio inventore eius quibusdam dignissimos architecto quam ea quia ex perferendis molestiae?</p>
+        </div>
+    )
+}
