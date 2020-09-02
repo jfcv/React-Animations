bbfbdf55f659c51832086d079500d4a7034750d9
velveet <velveet@pm.me>
2020-09-01 19:07:54 -0500

    number on Component1 react-spring animation added

--- src/components/Component1.jsx
+++ src/components/Component1.jsx
@@ -14,6 +14,17 @@ export default function Component1() {
                     <div style={c1Style}>
                         <h1>Component 1</h1>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur facilis eum sequi officia unde quis odit, rem et odio inventore eius quibusdam dignissimos architecto quam ea quia ex perferendis molestiae?</p>
+                        <Spring
+                        from= {{ number: 0 }}
+                        to= {{ number: 10 }}
+                        config= {{ duration: 10000 }}
+                        >
+                            {props => (
+                                <div style={props}>
+                                    <h1 style={counter}>{props.number.toFixed()}</h1>
+                                </div>
+                            )}
+                        </Spring>
                     </div>
                 </div>
             )}
@@ -27,4 +38,12 @@ const c1Style = {
     background: 'steelblue',
     color: 'white',
     padding: '1.5rem'
+}
+
+const counter = {
+    background: '#333',
+    textAlign: 'center',
+    width: '100px',
+    borderRadius: '50%',
+    margin: '1rem auto'
 }
